a4d2b328e6efca8e2725190336f876988ce489ef
anjarupnik <anjarupnik@hotmail.com>
2017-10-17 21:45:58 +0200

    working shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find(session[:cart_id])
+    else
+      Cart.new
+    end
+  end
+
+
 
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,12 @@
 class CartsController < ApplicationController
-  before_action :set_cart
   before_action :authenticate_user!
 
   def index
-    @cart_items = @cart.cart_items
-
+    @cart = current_cart
+    @cart_items = CartItem.all
   end
 
 
   private
-  def set_cart
-    @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    @cart = Cart.create
-    session[:cart_id] = @cart.id
-  end
+
 end

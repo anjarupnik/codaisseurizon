2dfb7b0f86444fd201bbd5d3119b15ba63509122
anjarupnik <anjarupnik@hotmail.com>
2017-10-18 20:40:50 +0200

    best cart ever

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,15 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
+  before_save :calculate_total
 
+
+private
   def calculate_total
-    self.cart_items.collect { |item| item.shoe.price * item.quantity }.sum
+    self.total = cart_items.collect { |item| item.shoe.price * item.quantity }.sum
   end
 
-  private
-  def update_total
-    self.total = calculate_total
-  end
 
 
 end

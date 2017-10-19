f2318eaed24b364c6308104a26088850160c25d9
FrisoDev <frisodevstoker@gmail.com>
2017-10-19 17:15:15 +0200

    fixed shopping button

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,9 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @cart = current_cart
+    @cart.save
+    session[:cart_id] = @cart.id
     @cart_items = current_cart.cart_items
   end
 

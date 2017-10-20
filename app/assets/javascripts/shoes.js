3c4eaceeb3311a04d70966759c93e12348ec82d0
anjarupnik <anjarupnik@hotmail.com>
2017-10-20 14:19:56 +0200

    moving destroy

--- app/assets/javascripts/shoes.js
+++ app/assets/javascripts/shoes.js
@@ -1,1 +1,15 @@
+function createItem(event) {
+  event.preventDefault();
+  $.ajax({
+  type: "POST",
+  url: "/todos.json",
+  data: JSON.stringify({
+    todo: newTodo
+  }),
+}
 
+
+
+$(document).ready(function() {
+  $("#addtocart").bind('click', createItem);
+});
